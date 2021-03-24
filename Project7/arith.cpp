5c7f0ccdba156a50f5e57ed9d89f4697a22c612f
Миха Непейцев <mihanep997@gmail.com>
2021-03-24 16:22:59 +0300

    arith

--- Project7/arith.cpp
+++ Project7/arith.cpp
@@ -1,18 +1,39 @@
 #include "arith.h"
 #include "math.h"
+#include <vector>
 bool arith(int a)
 {
-	if (a < 10) return false;
-	int k =  (a % 10) - (a / 10) % 10 ;
-	a /= 100;
-	int d=0;
-	while (a>=10) {
-		d =  (a % 10) - (a / 10) % 10 ;
-		if (k == d) {
-			k = d; a /= 100;
-		}
-		else return false;
+	int delta = 0;
+	int delta2 = 0;
+	std::vector<int> d;
+	while (a > 0) {
+		d.push_back(a % 10);
+		a /= 10;
 	}
-	
-    return true;
+	int i = 0;
+	if (d.size() == 1||d.size() == 0) return false;
+	if (d.size() == 2) return true;
+	while(i<d.size())
+	{
+		delta = d[i] - d[++i];
+		delta2 = d[i] - d[i+1];
+		if (delta != delta2) return false;
+	}
+	return true;
 }
+
+
+	/*int delta=0;
+	int i = 10;
+	while (a >= 1) {
+		delta
+		if (i != 10) {
+
+
+			double delta2 = a % i - a % (i * 10);
+
+		}
+
+		i = i * 10;
+	}*/
+
